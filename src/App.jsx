cadf2697e57bd5c9f3ebaf18c53329c32926304e
Yasmim Cassez <cassezyasmim@gmail.com>
2024-04-19 14:42:26 -0300

    Rotas routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import './App.css'
-import Home from './pages/Home/Home';
-import Animais from './pages/Animais/Animais';
-import { BrowserRouter as Roteador, Routes, Route } from 'react-router-dom'
-import Depoimentos from './components/Depoimentos/Depoimentos';
-import Fotos from './components/Fotos/Fotos';
-import Login from './components/Login/Login';
+import AppRoutes from './routes';
 
 function App() {
 
   return (
     <>
-     <Roteador>
-      <Routes>
-        <Route exact path='/' Component={Home} />
-        <Route path='/animais' Component={Animais} />
-        <Route path='/depoimentos' Component={Depoimentos} />
-        <Route path='/fotos' Component={Fotos} />
-        <Route path='/login' Component={Login} />
-      </Routes>
-     </Roteador>
+     <AppRoutes />
     </>
   )
 }
+import AppRoutes from './routes';
 
 export default App;
